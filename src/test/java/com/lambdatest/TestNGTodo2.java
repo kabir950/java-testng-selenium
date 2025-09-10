bf40fda7476bde6a2cfe128fde71d21fe00ba3d1
kabir950 <158033419+kabir950@users.noreply.github.com>
2025-09-11 00:01:27 +0530

    TestNGTodo2.java

--- src/test/java/com/lambdatest/TestNGTodo2.java
+++ src/test/java/com/lambdatest/TestNGTodo2.java
@@ -1,84 +1,80 @@
 package com.lambdatest;
 
+import java.io.File;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestNGTodo2 {
 
     private RemoteWebDriver driver;
-    private String Status = "passed";
+    private String Status = "failed";
 
     @BeforeMethod
     public void setup(Method m, ITestContext ctx) throws MalformedURLException {
-        String username = "kabirk";
-        String authkey = "LT_RhMHqS2TJ4lYNmnzOfTYRaNYNdbFdvoDAKSFTVknI2UQBth";
-        String hub = "hub.lambdatest.com/wd/hub";
+        String username = "ankitclambdatest";
+        String authkey = "LT_OnEL30AtoPH0BkcY83K0VnpQOcfFpX71axsfQgHGLhvNfUP";
+        String hub = "@hub.lambdatest.com/wd/hub";
 
         ChromeOptions browserOptions = new ChromeOptions();
-
-        // ✅ Fixed: use setCapability instead of invalid methods
-        browserOptions.setCapability("platformName", "Windows 11");
-        browserOptions.setCapability("browserVersion", "134.0");
-
         HashMap<String, Object> ltOptions = new HashMap<>();
+
         ltOptions.put("username", username);
         ltOptions.put("accessKey", authkey);
-        ltOptions.put("project", "Untitled");
-        ltOptions.put("selenium_version", "4.0.0");
-        ltOptions.put("build", "pop-up");
-        ltOptions.put("visual", true);
+        ltOptions.put("platformName", "Windows 10");
+        ltOptions.put("browserName", "Chrome");
+        ltOptions.put("browserVersion", "139.0");
+        ltOptions.put("build", "Build");
+        ltOptions.put("name", m.getName() + " - " + this.getClass().getSimpleName());
         ltOptions.put("w3c", true);
-        ltOptions.put("tags", new String[]{"Feature", "Falcon", "Severe"});
 
         browserOptions.setCapability("LT:Options", ltOptions);
 
-        driver = new RemoteWebDriver(
-                new URL("https://" + username + ":" + authkey + "@" + hub),
-                browserOptions
-        );
+        driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), browserOptions);
     }
 
-    @Test
-    public void testGoogleLuckyButton() throws InterruptedException {
-        driver.get("https://www.google.com/");
+    // Run this test multiple times if needed
+    @Test(invocationCount = 1)
+    public void basicTest() throws InterruptedException {
+        System.out.println("Loading Url for iteration...");
+        driver.get("https://di-review.tmp.phenom.pub/admin/journals");
+
         Thread.sleep(3000);
 
-        // Accept cookies if necessary (handle regional variations)
-        try {
-            driver.findElement(By.xpath("//div[text()='I agree']")).click();
-            Thread.sleep(1000);
-        } catch (Exception ignored) {}
+        driver.findElement(By.xpath("//*[@id='username']")).sendKeys("ankit");
 
-        // ❌ Incorrect XPath, fixed by navigating directly
-        driver.get("https://www.crunchyroll.com");
-        Thread.sleep(5000);
+        Thread.sleep(3000);
 
-        // Get the new URL and print to console
-        String currentURL = driver.getCurrentUrl();
-        System.out.println("New URL after clicking: " + currentURL);
+        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("1234");
 
-        // Open the URL in a new tab
-        ((JavascriptExecutor) driver).executeScript("window.open('" + currentURL + "', '_blank');");
 
-        Thread.sleep(5000);
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+
+        Status = "passed";
+        System.out.println("Iteration finished successfully");
     }
 
     @AfterMethod
     public void tearDown() {
-        if (driver != null) {
-            driver.executeScript("lambda-status=" + Status);
-            driver.quit();
-        }
+        driver.executeScript("lambdatest_executor: {\"action\": \"stepcontext\", \"arguments\": {\"data\": \"Adding Test Result and Closing Browser\", \"level\": \"info\"}}");
+        driver.executeScript("lambda-status=" + Status);
+        driver.quit();
     }
 }
+
+
+
+
+
